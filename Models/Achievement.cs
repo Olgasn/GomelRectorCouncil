88d54d6fb1d73354488658052a15b008baaf0a72
Асенчик Олег Даниилович <olgasn@inbox.ru>
2017-05-03 09:56:59 +0300

    Показатели

--- Models/Achievement.cs
+++ Models/Achievement.cs
@@ -13,6 +13,7 @@ namespace GomelRectorCouncil.Models
         public int AchievementId {get; set;}
         public int IndicatorId {get; set;}
         public int UnivercityId {get; set;}
+        public float IndicatorValue {get; set;}
         public University Univercity {get; set;}
         public Indicator Indicator {get; set;}
     }
--- Models/Indicator.cs
+++ Models/Indicator.cs
@@ -7,16 +7,33 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GomelRectorCouncil.Models
 {
+    public enum IndicatorType
+        {
+        text, int_number, float_number
+        }
     public class Indicator
     {
+        
         [Key]
         public int IndicatorId {get; set;}
-        public string IndicatorName {get; set;}
-        public string IndicatorDescription {get; set;}
+
+        public int IndicatorId1 {get; set;}
+        public int IndicatorId2 {get; set;}
+        public int IndicatorId3 {get; set;}
+
+        public int IndicatorValue {get; set;}
         
+        public string IndicatorName {get; set;}
+
+        [DisplayFormat(NullDisplayText = "-")]
+        public IndicatorType? IndicatorType { get; set; }
+
+        public string IndicatorDescription {get; set;}
+
         [Required]
         [Range(2010, 2050, ErrorMessage = "Недопустимый год")]
         public int Year {get; set;}
+
 
         public ICollection<Achievement> Achievements {get; set;}
 
