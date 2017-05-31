c2eb1256eb07792aa327004914fc59e3b51670c9
olgasn@inbox.ru <olgasn@inbox.ru>
2017-05-31 11:14:43 +0300

    Показатели - список лет

--- Areas/Admin/ViewModels/IndicatorViewModel.cs
+++ Areas/Admin/ViewModels/IndicatorViewModel.cs
@@ -3,14 +3,26 @@ using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using GomelRectorCouncil.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace GomelRectorCouncil.Areas.Admin.ViewModels
 {
     public class IndicatorViewModel
     {
-        public IEnumerable<Indicator> Indicators { get; set; }
-        [Display(Name="Заданный год")]
+        public IEnumerable<Indicator> Indicators{get;set;}
+ 
+        [Display(Name="Выберите год")]
         public int CurrentYear { get; set; }
+        public SelectList ListYears 
+        { 
+            get
+            {
+                SelectList ls=new SelectList(Indicators.Where(f=>f.Year==CurrentYear).Select(f=>f.Year).Distinct());
+                return ls;
+
+            }
+        }
+
 
     }
 }
