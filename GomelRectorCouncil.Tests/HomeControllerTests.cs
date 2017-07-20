40246121457578adc08efc813cf296c49554cd73
olgasn <olgasn@inbox.ru>
2017-07-20 21:13:56 +0300

    Реорганизация каталогов

---
+++ GomelRectorCouncil.Tests/HomeControllerTests.cs
@@ -0,0 +1,46 @@
+﻿using GomelRectorCouncil.Controllers;
+using GomelRectorCouncil.Data;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace GomelRectorCouncil.Tests
+{
+    public class HomeControllerTests
+    {
+
+        [Fact]
+        public void IndexViewDataMessage(CouncilDbContext context)
+        {
+            // Arrange
+            HomeController controller = new HomeController(context);
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.Equal("Hello world!", result?.ViewData["Message"]);
+        }
+
+        [Fact]
+        public void IndexViewResultNotNull(CouncilDbContext context)
+        {
+            // Arrange
+            HomeController controller = new HomeController(context);
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void IndexViewNameEqualIndex(CouncilDbContext context)
+        {
+            // Arrange
+            HomeController controller = new HomeController(context);
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+            // Assert
+            Assert.Equal("Index", result?.ViewName);
+        }
+    }
+}
